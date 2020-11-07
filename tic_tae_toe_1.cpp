33c7397e1b773142c1378bcffe1f4d262999b1b4
kostakis45 <kostakis45@gmail.com>
2020-11-07 21:39:11 +0200

    Update tic_tae_toe_1.cpp

--- tic_tae_toe_1.cpp
+++ tic_tae_toe_1.cpp
@@ -5,59 +5,114 @@
 
 using namespace std;
 char arr[9] = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
-char again = ' ';
+char again;
 const char player_1 = 'X', player_2 = 'O';
-int choose = 0;
-int player_1_temp, player_2_temp, dice, count = 0, ds = 0, winner = 0;
+int player_1_temp, player_2_temp, ds = 0, winner = 0, choose = 0, count;
+void display();
+void dice();
+void turns();
+void check_winner();
 void again_or_not();
 
-void dice_event()
+int main()
+{
+    cout << "~~To decide who will play first please roll the dice.~~\n"
+         << endl;
+    display();
+    dice();
+    again_or_not();
+    system("pause");
+    return 0;
+}
+
+void display()
+{
+    cout << "_" << arr[0] << "_|_" << arr[1] << "_|_" << arr[2] << "_" << endl;
+    cout << "_" << arr[3] << "_|_" << arr[4] << "_|_" << arr[5] << "_" << endl;
+    cout << " " << arr[6] << " | " << arr[7] << " | " << arr[8] << endl;
+}
+
+void dice()
 {
     srand(time(NULL));
 
     if (ds == 0)
     {
-        cout << "Player 1 roll the dice." << endl;
-        dice = rand() % 6 + 1;
-        player_1_temp = dice;
-        cout << "Player 1 rolled: " << dice << endl
-             << endl;
-        cout << "Player 2 roll the dice." << endl;
-        dice = rand() % 6 + 1;
-        player_2_temp = dice;
-        cout << "Player 2 rolled: " << dice << endl
-             << endl;
-    }
+        while (player_1_temp == player_2_temp)
+        {
+            cout << "Player 1 roll the dice." << endl;
+            player_1_temp = rand() % 6 + 1;
+            cout << "Player 1 rolled: " << player_1_temp << endl
+                 << endl;
+            cout << "Player 2 roll the dice." << endl;
+            player_2_temp = rand() % 6 + 1;
+            cout << "Player 2 rolled: " << player_2_temp << endl
+                 << endl;
+        }
 
-    if (player_1_temp > player_2_temp)
-    {
-        cout << "Player 1 plays first.\n"
-             << endl;
-    }
-    else if (player_1_temp < player_2_temp)
-    {
-        cout << "Player 2 plays first.\n"
-             << endl;
-    }
-    else if (player_1_temp == player_2_temp)
-    {
-        dice_event();
-    }
+        if (player_1_temp > player_2_temp)
+        {
+            cout << "Player 1 plays first.\n"
+                 << endl;
+            count = 0;
+            turns();
+        }
+        else if (player_1_temp < player_2_temp)
+        {
+            cout << "Player 2 plays first.\n"
+                 << endl;
+            count = 1;
+            turns();
+        }
+        else if (player_1_temp == player_2_temp)
+        {
+            system("cls");
+            display();
+        }
 
-    ds++;
+        ds++;
+    }
 }
 
-void display()
+void turns()
 {
-    system("cls");
+    while (winner != 1)
+    {
+        if (count == 0)
+        {
+            do
+            {
+                cout << "Player 1 pick a number: ";
+                cin >> choose;
+            } while (choose < 0 || choose > 8);
 
-    cout << "~~To decide who will play first please roll the dice.~~\n"
-         << endl;
-    dice_event();
-    cout << "_" << arr[0] << "_|_" << arr[1] << "_|_" << arr[2] << "_" << endl;
-    cout << "_" << arr[3] << "_|_" << arr[4] << "_|_" << arr[5] << "_" << endl;
-    cout << " " << arr[6] << " | " << arr[7] << " | " << arr[8] << endl;
+            arr[choose] = player_1;
+            choose = 0;
+            count++;
+            system("cls");
+            check_winner();
+            display();
+        }
+        else if (count == 1)
+        {
+            do
+            {
+                cout << "Player 2 pick a number: ";
+                cin >> choose;
+            } while (choose < 0 || choose > 8);
 
+            arr[choose] = player_2;
+            choose = 0;
+            count--;
+            system("cls");
+            check_winner();
+            display();
+        }
+    }
+}
+
+void check_winner()
+{
     if (arr[0] == 'X' && arr[4] == 'X' && arr[8] == 'X')
     {
         cout << "\nPLAYER 1 IS THE WINNER.\n"
@@ -169,75 +224,30 @@ void display()
     }
 }
 
-void player_1_f()
-{
-    do
-    {
-        cout << "Player 1 pick a number: ";
-        cin >> choose;
-    } while (choose < 0 || choose > 8);
-
-    arr[choose] = player_1;
-    choose = 0;
-}
-
-void player_2_f()
-{
-    do
-    {
-        cout << "Player 2 pick a number: ";
-        cin >> choose;
-    } while (choose < 0 || choose > 8);
-
-    arr[choose] = player_2;
-    choose = 0;
-}
-
-int main()
-{
-    display();
-
-    while (winner != 1)
-    {
-        if (count == 0 && player_1_temp > player_2_temp)
-        {
-            player_1_f();
-            display();
-            count++;
-        }
-        else if (count == 1)
-        {
-            player_2_f();
-            display();
-            count--;
-        }
-    }
-
-    again_or_not();
-    system("pause");
-    return 0;
-}
-
 void again_or_not()
 {
-    cout << "Would you like to play again? ";
+    cout << "Would you like to play again? (Y/N): ";
     cin >> again;
 
-    while (again != 'Y' || again != 'y' || again != 'N' || again != 'o')
+    if (again != 'Y' && again != 'y' && again != 'N' && again != 'n')
     {
-        cout << "Wrong letter.Try again" << endl;
+        cout << "Wrong letter.Try again: ";
+        cin >> again;
+    }
 
-        if (again == 'Y' || again == 'y')
-        {
-            ds = 0;
-            winner = 0;
-            count = 0;
-            choose = 0;
-            main();
-        }
-        else if (again == 'N' || again == 'n')
-        {
-            exit(EXIT_SUCCESS);
-        }
+    if (again == 'Y' || again == 'y')
+    {
+        ds = 0;
+        winner = 0;
+        count = 0;
+        choose = 0;
+        player_1_temp = 0;
+        player_2_temp = 0;
+        system("cls");
+        main();
+    }
+    else if (again == 'N' || again == 'n')
+    {
+        exit(EXIT_SUCCESS);
     }
 }
